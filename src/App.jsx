f4ef75dcdf91304f8467d5081b99442c4cea5911
Loïc Zeiss <loiczeiss@gmail.com>
2023-09-27 18:11:15 +0200

    js working, got to work on the error fctns

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,43 @@ function App() {
   const [inputMonth, setinputMonth] = useState('')
   const [inputYear, setInputYear] = useState('')
 
-  const Today = new Date()
-  console.log(Today)
-console.log(inputDay)
+  const dateInput = new Date(`${inputYear},${inputMonth},${inputDay}`).getTime()
+  
+  const dateToday = new Date().getTime()
+  
+  const milliseconds = dateToday-dateInput
+
+function msToYearsMonthsDays() {
+
+  const msPerDay = 24 * 60 * 60 * 1000; // milliseconds in a day
+  const msPerYear = msPerDay * 365.25; // average milliseconds in a year
+
+  setYear(Math.floor(milliseconds / msPerYear)) ;
+  const remainingMilliseconds = milliseconds % msPerYear;
+  setMonth(Math.floor(remainingMilliseconds / (msPerDay * 30.44))); // average days in a month
+  setDay(Math.floor((remainingMilliseconds % (msPerDay * 30.44)) / msPerDay));
+
+}
+function isDateValid(day, month, year) {
+  const date = new Date(`${year}-${month}-${day}`);
+  return !isNaN(date.getTime()) && date.getDate() == day && date.getMonth() + 1 == month && date.getFullYear() == year;
+}
+function handleButtonClick() {
+  const isInputValid = isDateValid(inputDay, inputMonth, inputYear);
+
+  if (!isInputValid ) {
+    // Show an error message to the user, e.g., set an error state
+    // You can also display a message in the UI
+    alert("Invalid date. Please entergerg a valid date.");
+    return;
+  } 
+
+  // If the date is valid, you can proceed with your calculations
+  msToYearsMonthsDays(milliseconds);
+}
+
+
+
 // const calculate = () => {
   
 //   console.log(dayInput)
@@ -24,10 +58,8 @@ console.log(inputDay)
       <div className="flex h-screen w-screen bg-OffWhite items-center justify-center">
         <main className="flex bg-white w-10/12 sm:w-10/12 xl:w-7/12 xl:h-9/12 rounded-t-xl rounded-bl-xl rounded-br-[100px] md:rounded-br-[150px] flex-col">
           <section className="m-4 sm:m-12 mb-0 w-full">
-            <form
-              action=""
-              className="w-11/12 sm:w-11/12 md:w-10/12 lg:w-8/12 xl:w-11/12"
-            >
+           <div
+              className="w-11/12 sm:w-11/12 md:w-10/12 lg:w-8/12 xl:w-11/12">
               <article className="flex h-min w-full sm:w-8/12 lg:w-6/12 xl:w-8/12 justify-start mb-8 sm:mb-12">
                 <div className="flex flex-col mr-4 sm:mr-6">
                   <label
@@ -82,12 +114,12 @@ console.log(inputDay)
               <span className="flex flex-col items-center sm:flex-row sm:items-center">
                 <hr className="w-full xl:w-11/12 mb-4 sm:mb-0" />
                 <button 
-                // onClick={calculate}
+                onClick={()=> handleButtonClick()}
                 className="bg-Purple w-12 h-12 -mt-10 md:mt-0 rounded-full xl:h-16 xl:w-16 flex items-center justify-center focus:bg-black">
                   <img className="w-1/2 h-1/2" src={Icon} alt="" />
                 </button>
               </span>
-            </form>
+            </div>
           </section>
           <section className="flex flex-col mx-4 sm:mx-12 mb-12 text-[40px] md:text-[60px] font-extrabold font-poppins justify-center h-full italic">
             <p>
